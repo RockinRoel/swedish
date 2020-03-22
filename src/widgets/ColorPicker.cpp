2d7403c5e898ea95e62f358e960b3765f957a079
Roel Standaert <roel@abittechnical.com>
2020-03-22 09:04:47 +0100

    Color picker using jscolor

--- src/widgets/ColorPicker.cpp
+++ src/widgets/ColorPicker.cpp
@@ -1,105 +1,49 @@
 #include "ColorPicker.h"
 
-#include <Wt/WColor.h>
-#include <Wt/WContainerWidget.h>
-#include <Wt/WCssDecorationStyle.h>
-#include <Wt/WLength.h>
-#include <Wt/WSlider.h>
-#include <Wt/WSpinBox.h>
-#include <Wt/WTemplate.h>
+#include <Wt/WApplication.h>
+#include <Wt/WStringStream.h>
 
-#include <cmath>
-
-namespace {
-
-Wt::WColor randomColor()
-{
-  // TODO(Roel): random color
-  return Wt::WColor(Wt::StandardColor::Red);
-}
-
-}
+#include <cstdlib>
+#include <iostream>
 
 namespace swedish {
 
-ColorPicker::ColorPicker(const Wt::WColor &color)
-  : Wt::WCompositeWidget(std::make_unique<Wt::WTemplate>(tr("tpl.swedish.colorpicker"))),
-    pickedColor_(color),
-    hueSlider_(impl()->bindNew<Wt::WSlider>("hue_slider")),
-    saturationSlider_(impl()->bindNew<Wt::WSlider>("saturation_slider")),
-    lightnessSlider_(impl()->bindNew<Wt::WSlider>("lightness_slider")),
-    hueSpinbox_(impl()->bindNew<Wt::WSpinBox>("hue_spinbox")),
-    saturationSpinbox_(impl()->bindNew<Wt::WSpinBox>("saturation_spinbox")),
-    lightnessSpinbox_(impl()->bindNew<Wt::WSpinBox>("lightness_spinbox")),
-    currentColor_(impl()->bindNew<Wt::WContainerWidget>("current_color"))
+ColorPicker::ColorPicker(Wt::WColor color)
 {
-  currentColor_->resize(15, 15);
+  Wt::WApplication *app = Wt::WApplication::instance();
+  app->require("/js/jscolor.js");
 
-  hueSlider_->setRange(0, 360);
-  hueSpinbox_->setRange(0, 360);
-  saturationSlider_->setRange(0, 100);
-  saturationSpinbox_->setRange(0, 100);
-  lightnessSlider_->setRange(0, 100);
-  lightnessSpinbox_->setRange(0, 100);
+  addStyleClass("jscolor");
 
-  if (pickedColor_.isDefault()) {
-    pickedColor_ = randomColor();
-  }
+  if (color.isDefault())
+    color = Wt::WColor(Wt::StandardColor::Red);
 
-  double hsl[3];
-  pickedColor_.toHSL(hsl);
-  const int h = static_cast<int>(std::round(hsl[0]));
-  const int s = static_cast<int>(std::round(hsl[1] * 100.0));
-  const int l = static_cast<int>(std::round(hsl[2] * 100.0));
-  hueSlider_->setValue(h);
-  hueSpinbox_->setValue(h);
-  saturationSlider_->setValue(s);
-  saturationSpinbox_->setValue(s);
-  lightnessSlider_->setValue(l);
-  lightnessSpinbox_->setValue(l);
-
-  currentColor_->decorationStyle().setBackgroundColor(pickedColor_);
-
-  hueSlider_->valueChanged().connect(this, std::bind(&ColorPicker::sliderChanged, this, hueSlider_, hueSpinbox_));
-  saturationSlider_->valueChanged().connect(this, std::bind(&ColorPicker::sliderChanged, this, saturationSlider_, saturationSpinbox_));
-  lightnessSlider_->valueChanged().connect(this, std::bind(&ColorPicker::sliderChanged, this, lightnessSlider_, lightnessSpinbox_));
-
-  hueSpinbox_->changed().connect(this, std::bind(&ColorPicker::spinboxChanged, this, hueSlider_, hueSpinbox_));
-  saturationSpinbox_->changed().connect(this, std::bind(&ColorPicker::spinboxChanged, this, saturationSlider_, saturationSpinbox_));
-  lightnessSpinbox_->changed().connect(this, std::bind(&ColorPicker::spinboxChanged, this, lightnessSlider_, lightnessSpinbox_));
+  Wt::WStringStream js;
+  js << "jscolor.installByClassName('jscolor');"
+     << jsRef() + ".jscolor.fromRGB("
+     << color.red() << ','
+     << color.green() << ','
+     << color.blue() << ");";
+  doJavaScript(js.str());
 }
 
 ColorPicker::~ColorPicker()
 { }
 
-Wt::WTemplate *ColorPicker::impl()
+const Wt::WColor ColorPicker::pickedColor() const
 {
-  return static_cast<Wt::WTemplate*>(implementation());
-}
+  const std::string s = text().toUTF8();
+  char *end = nullptr;
+  long rgb = std::strtol(s.c_str(), &end, 16);
 
-void ColorPicker::sliderChanged(const Wt::WSlider *slider, Wt::WSpinBox *spinBox)
-{
-  spinBox->setValue(slider->value());
+  if (end != &*s.end())
+    return Wt::WColor();
 
-  valueChanged();
-}
-void ColorPicker::spinboxChanged(Wt::WSlider *slider, const Wt::WSpinBox *spinBox)
-{
-  slider->setValue(spinBox->value());
+  const int b = rgb & 0xFF;
+  const int g = (rgb >> 8) & 0xFF;
+  const int r = (rgb >> 16) & 0xFF;
 
-  valueChanged();
-}
-
-void ColorPicker::valueChanged()
-{
-  const double h = hueSlider_->value();
-  const double s = saturationSlider_->value() / 100.0;
-  const double l = lightnessSlider_->value() / 100.0;
-  pickedColor_ = Wt::WColor::fromHSL(h, s, l, 255);
-
-  currentColor_->decorationStyle().setBackgroundColor(pickedColor_);
-
-  colorChanged_(pickedColor_);
+  return Wt::WColor(r, g, b);
 }
 
 }
