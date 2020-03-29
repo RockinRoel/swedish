ab147c313a928ffad53b389b1f340a2b6464ea63
Roel Standaert <roel@abittechnical.com>
2020-03-29 16:22:08 +0200

    Added run script

---
+++ scripts/run.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+dir=$(cd -P -- "$(dirname -- "$0")" && pwd -P)
+cd ${dir}
+exec ./swedish.wt --docroot "docroot;/css,/js,/puzzles,/resources" --approot approot -c approot/wt_config.xml --http-listen 0.0.0.0:1234 --resources-dir /home/roel/projects/wt/git/wt/resources
