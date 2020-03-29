f70c59baa948d764a7c3b96dcff55250564cf87a
Roel Standaert <roel@abittechnical.com>
2020-03-29 16:24:44 +0200

    run.sh: start from dir above bin dir

--- scripts/run.sh
+++ scripts/run.sh
@@ -2,4 +2,5 @@
 
 dir=$(cd -P -- "$(dirname -- "$0")" && pwd -P)
 cd ${dir}
-exec ./swedish.wt --docroot "docroot;/css,/js,/puzzles,/resources" --approot approot -c approot/wt_config.xml --http-listen 0.0.0.0:1234 --resources-dir /home/roel/projects/wt/git/wt/resources
+cd ..
+exec ./bin/swedish.wt --docroot "docroot;/css,/js,/puzzles,/resources" --approot approot -c approot/wt_config.xml --http-listen 0.0.0.0:1234 --resources-dir /home/roel/projects/wt/git/wt/resources
