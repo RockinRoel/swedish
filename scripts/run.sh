c816442e8ae29fb96847c2827220aefdbd8a9667
Roel Standaert <roel@emweb.be>
2020-12-02 15:17:59 +0100

    Fixed and formatted run params

--- scripts/run.sh
+++ scripts/run.sh
@@ -3,4 +3,9 @@
 dir=$(cd -P -- "$(dirname -- "$0")" && pwd -P)
 cd ${dir}
 cd ..
-exec ./bin/swedish.wt --docroot "docroot;/css,/js,/puzzles,/resources" --approot approot -c approot/wt_config.xml --http-listen 0.0.0.0:1234 --resources-dir /home/roel/projects/wt/git/wt/resources
+exec ./bin/swedish.wt \
+  --docroot "docroot;/css,/js,/puzzles,/resources" \
+  --approot approot \
+  -c approot/wt_config.xml \
+  --http-listen 0.0.0.0:1234 \
+  --resources-dir /home/roel/project/wt/git/wt4/install-dir/static/share/Wt/resources
